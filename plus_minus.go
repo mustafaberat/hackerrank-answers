011e0cf253cfa8c2ce84edd8ef8500829a1b1d1f
Mustafa Berat <32976555+mustafaberat@users.noreply.github.com>
2021-11-21 23:43:02 +0300

    Update plus_minus.go
    add extra solution

--- plus_minus.go
+++ plus_minus.go
@@ -20,3 +20,52 @@ func plusMinus(arr []int32) {
     fmt.Println(n/float32(l))
     fmt.Println(z/float32(l))
 }
+
+// ______________________
+// ______________________
+// ______________________
+// ______________________
+// ______________________
+// ______________________
+
+package algorithms
+
+import (
+	"testing"
+)
+
+func Test_PlustMinus(t *testing.T) {
+	inputs := [][]int{
+		{1, 1, 0, -1, -1},
+		{-4, 3, -9, 0, 4, 1},
+	}
+	results := [][]float32{
+		{0.40, 0.40, 0.20},
+		{0.50, 0.33, 0.16},
+	}
+	for i := 0; i < len(inputs); i++ {
+		pl, nl, zl := plusMinus(inputs[i])
+		if !(pl == results[i][0] || nl == results[i][1] || zl == results[i][2]) {
+			t.Error("Not correct result!")
+		}
+	}
+
+}
+
+func plusMinus(input []int) (float32, float32, float32) {
+	var (
+		p, n, z float32 = 0, 0, 0
+		l               = len(input)
+	)
+
+	for i := 0; i < l; i++ {
+		if input[i] == 0 {
+			z++
+		} else if input[i] > 0 {
+			p++
+		} else if input[i] < 0 {
+			n++
+		}
+	}
+	return p / float32(l), n / float32(l), z / float32(l)
+}
