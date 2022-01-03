990243ae206aba2062ed19a54f316c03ca75bfea
Mustafa Berat <32976555+mustafaberat@users.noreply.github.com>
2022-01-03 23:27:47 +0300

    Update cut_the_sticks_test.go

--- cut_the_sticks_test.go
+++ cut_the_sticks_test.go
@@ -1,4 +1,37 @@
+package algorithms
 
+import (
+	"testing"
+)
+
+func TestCutTheSticks(t *testing.T) {
+	var (
+		want  []int32
+		input []int32
+		got   []int32
+	)
+
+	want = []int32{3, 2, 1}
+	input = []int32{1, 2, 3}
+	got = cutTheSticks(input)
+	if isDifferent(got, want) {
+		t.Errorf("Not correct. Input: %v Got: %v Want: %v \n", input, got, want)
+	}
+
+	want = []int32{6, 4, 2, 1}
+	input = []int32{5, 4, 4, 2, 2, 8}
+	got = cutTheSticks(input)
+	if isDifferent(got, want) {
+		t.Errorf("Not correct. Input: %v Got: %v Want: %v \n", input, got, want)
+	}
+
+	want = []int32{8, 6, 4, 1}
+	input = []int32{1, 2, 3, 4, 3, 3, 2, 1}
+	got = cutTheSticks(input)
+	if isDifferent(got, want) {
+		t.Errorf("Not correct. Input: %v Got: %v Want: %v \n", input, got, want)
+	}
+}
 
 func cutTheSticks(arr []int32) []int32 {
 	var cutLen []int32
