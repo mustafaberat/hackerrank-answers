f28c1941f3788c6dad93d48f3832ba0ac83fa69d
Mustafa Berat <32976555+mustafaberat@users.noreply.github.com>
2022-01-05 22:57:45 +0300

    Create counting_valleys_test.go

---
+++ counting_valleys_test.go
@@ -0,0 +1,47 @@
+package algorithms
+
+import (
+	"testing"
+)
+
+func TestCountingValleys(t *testing.T) {
+	var (
+		want int32
+		got  int32
+	)
+
+	want = 1
+	got = countingValleys("UDDDUDUU")
+	if got != want {
+		t.Errorf("Not correct. Got: %v Want: %v \n", got, want)
+	}
+
+	want = 0
+	got = countingValleys("UDUUUDUDDD")
+	if got != want {
+		t.Errorf("Not correct. Got: %v Want: %v \n", got, want)
+	}
+
+	want = 2
+	got = countingValleys("DDUUDDDUDUUDUDDDUUDDUDDDUDDDUDUUDDUUDDDUDDDUDDDUUUDUDDDUDUDUDUUDDUDUDUDUDUUUUDDUDDUUDUUDUUDUUUUUUUUU")
+	if got != want {
+		t.Errorf("Not correct. Got: %v Want: %v \n", got, want)
+	}
+}
+
+func countingValleys(path string) int32 {
+	var lvl, seaLvlCounter int32
+
+	for i, l := 0, len(path); i < l; i++ {
+		if path[i] == 'D' {
+			if lvl == 0 {
+				seaLvlCounter++
+			}
+			lvl -= 1
+		} else if path[i] == 'U' {
+			lvl += 1
+		}
+	}
+
+	return seaLvlCounter
+}
